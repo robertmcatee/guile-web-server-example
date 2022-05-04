8332d67b7f043c12ac57a2c1118eb75a76ef6f6b
Robert McAtee <rtmcatee@gmail.com>
2022-05-04 01:24:09 -0500

    Add files via upload

---
+++ hello-world.scm
@@ -0,0 +1,61 @@
+(use-modules (web server))
+(use-modules (web request)
+             (web response)
+             (web uri)
+	     (sxml simple))
+
+(define (templatize title body)
+  `(html (head (title ,title))
+         (body ,@body)))
+
+(define* (respond #:optional body #:key
+                  (status 200)
+                  (title "Hello hello!")
+                  (doctype "<!DOCTYPE html>\n")
+                  (content-type-params '((charset . "utf-8")))
+                  (content-type 'text/html)
+                  (extra-headers '())
+                  (sxml (and body (templatize title body))))
+  (values (build-response
+           #:code status
+           #:headers `((content-type
+                        . (,content-type ,@content-type-params))
+                       ,@extra-headers))
+          (lambda (port)
+            (if sxml
+                (begin
+                  (if doctype (display doctype port))
+                  (sxml->xml sxml port))))))
+
+(define (request-path-components request)
+  (split-and-decode-uri-path (uri-path (request-uri request))))
+
+(define (not-found request)
+  (values (build-response #:code 404)
+          (string-append "Resource not found: "
+                         (uri->string (request-uri request)))))
+
+(define (debug-page request body)
+  (if (equal? (request-path-components request)
+              '("hacker"))
+      (values '((content-type . (text/plain)))
+              "Hello hacker!")
+      (not-found request))
+
+  (if (null? (request-path-components request))
+      (respond
+       `((h1 "hello world!")
+	 (table
+	  (tr (th "header") (th "value"))
+	  ,@(map (lambda (pair)
+		   `(tr (td (tt ,(with-output-to-string
+				   (lambda () (display (car pair))))))
+			(td (tt ,(with-output-to-string
+				   (lambda ()
+				     (write (cdr pair))))))))
+		 (request-headers request)))))
+      (not-found request)))
+
+(run-server debug-page)
+;;(run-server hello-hacker-handler)
+;;(run-server hello-world-handler)
