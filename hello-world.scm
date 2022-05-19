274b55cda0a2ecf79dcfdada2f505cd7e381ccb6
Robert McAtee <rtmcatee@gmail.com>
2022-05-19 15:24:47 -0500

    Update hello-world.scm
    purged more spurious tabs.

--- hello-world.scm
+++ hello-world.scm
@@ -1,39 +1,39 @@
 (use-modules (web server))
 (use-modules (web request)
-	     (web response)
-	     (web uri)
-	     (sxml simple))
+             (web response)
+             (web uri)
+             (sxml simple))
 
 (define (templatize title body)
   `(html (head (title ,title))
          (body ,@body)))
 
 (define* (respond #:optional body #:key
-		  (status 200)
-		  (title "Hello hello!")
-		  (doctype "<!DOCTYPE html>\n")
-		  (content-type-params '((charset . "utf-8")))
-		  (content-type 'text/html)
-		  (extra-headers '())
-		  (sxml (and body (templatize title body))))
+                  (status 200)
+                  (title "Hello hello!")
+                  (doctype "<!DOCTYPE html>\n")
+                  (content-type-params '((charset . "utf-8")))
+                  (content-type 'text/html)
+                  (extra-headers '())
+                  (sxml (and body (templatize title body))))
   (values (build-response
-	   #:code status
-	   #:headers `((content-type
-			. (,content-type ,@content-type-params))
-		       ,@extra-headers))
-	  (lambda (port)
-	    (if sxml
-		(begin
-		  (if doctype (display doctype port))
-		  (sxml->xml sxml port))))))
+           #:code status
+           #:headers `((content-type
+                        . (,content-type ,@content-type-params))
+                       ,@extra-headers))
+          (lambda (port)
+            (if sxml
+                (begin
+                  (if doctype (display doctype port))
+                  (sxml->xml sxml port))))))
 
 (define (request-path-components request)
   (split-and-decode-uri-path (uri-path (request-uri request))))
 
 (define (not-found request)
   (values (build-response #:code 404)
-	  (string-append "Resource not found: "
-			 (uri->string (request-uri request)))))
+          (string-append "Resource not found: "
+                         (uri->string (request-uri request)))))
 
 (define (site request body)
   (cond
@@ -41,26 +41,26 @@
     (respond
      `((h1 "hello world!!")
        (table
-	(tr (th "header") (th "value"))
-	,@(map (lambda (pair)
-		 `(tr (td (tt ,(with-output-to-string
-				 (lambda () (display (car pair))))))
-		      (td (tt ,(with-output-to-string
-				 (lambda ()
-				   (write (cdr pair))))))))
-	       (request-headers request))))))
+        (tr (th "header") (th "value"))
+        ,@(map (lambda (pair)
+                 `(tr (td (tt ,(with-output-to-string
+                                 (lambda () (display (car pair))))))
+                      (td (tt ,(with-output-to-string
+                                 (lambda ()
+                                   (write (cdr pair))))))))
+               (request-headers request))))))
    ((equal? (request-path-components request) '("hacker"))
     (respond
      `((h1 "hello hacker!!")
        (table
-	(tr (th "header") (th "value"))
-	,@(map (lambda (pair)
-		 `(tr (td (tt ,(with-output-to-string
-				 (lambda () (display (car pair))))))
-		      (td (tt ,(with-output-to-string
-				 (lambda ()
-				   (write (cdr pair))))))))
-	       (request-headers request))))))
+        (tr (th "header") (th "value"))
+        ,@(map (lambda (pair)
+                 `(tr (td (tt ,(with-output-to-string
+                                 (lambda () (display (car pair))))))
+                      (td (tt ,(with-output-to-string
+                                 (lambda ()
+                                   (write (cdr pair))))))))
+               (request-headers request))))))
    (else (not-found request))))
 
 (run-server site)
